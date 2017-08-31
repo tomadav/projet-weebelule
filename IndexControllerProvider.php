abc38fde882642d29c5d6073c5eb0f66e42a579f
Tomadav <30336934+tomadav@users.noreply.github.com>
2017-08-31 16:55:53 +0200

    31 aprem
    mise a jour de la fiche d'inscription avec auto-complete google-maps
    mise à jour sur les fichiers index

--- IndexControllerProvider.php
+++ IndexControllerProvider.php
@@ -25,6 +25,38 @@ class IndexControllerProvider implements ControllerProviderInterface {
                 # pour la créations de lien : "controller_action"
                 ->bind('index_index');
 
+            # Page des FAQ
+            $controllers
+                # On associe une Route à un Controller et une Action
+                ->get('/faq', 'App\Controller\IndexController::faqAction')
+                # En option je peux donner un nom à la route, qui servira plus tard
+                # pour la créations de lien : "controller_action"
+                ->bind('projet-weebelule_faq');
+
+            # Page des CGU
+            $controllers
+                # On associe une Route à un Controller et une Action
+                ->get('/cgu', 'App\Controller\IndexController::cguAction')
+                # En option je peux donner un nom à la route, qui servira plus tard
+                # pour la créations de lien : "controller_action"
+                ->bind('projet-weebelule_cgu');
+
+            # Page des Mentions légales
+            $controllers
+                # On associe une Route à un Controller et une Action
+                ->get('/mentions', 'App\Controller\IndexController::mentionsAction')
+                # En option je peux donner un nom à la route, qui servira plus tard
+                # pour la créations de lien : "controller_action"
+                ->bind('projet-weebelule_mentions');
+
+            # Page Comment ça marche
+            $controllers
+                # On associe une Route à un Controller et une Action
+                ->get('/ccm', 'App\Controller\IndexController::ccmAction')
+                # En option je peux donner un nom à la route, qui servira plus tard
+                # pour la créations de lien : "controller_action"
+                ->bind('projet-weebelule_ccm');
+
             # Page de connexion
             $controllers
                 # On associe une Route à un Controller et une Action
@@ -32,6 +64,15 @@ class IndexControllerProvider implements ControllerProviderInterface {
                 # En option je peux donner un nom à la route, qui servira plus tard
                 # pour la créations de lien : "controller_action"
                 ->bind('projet-weebelule_connexion');
+
+            # Page de contact
+            $controllers
+                # On associe une Route à un Controller et une Action
+                ->match('/contact', 'App\Controller\IndexController::contactAction')
+                # En option je peux donner un nom à la route, qui servira plus tard
+                # pour la créations de lien : "controller_action"
+                ->method('GET|POST')
+                ->bind('projet-weebelule_contact');
 
             # Page d'inscription
             $controllers
@@ -42,7 +83,7 @@ class IndexControllerProvider implements ControllerProviderInterface {
                 ->bind('projet-weebelule_inscription');
 
             $controllers
-                ->post('/inscription', 'weebelule\ControllerIndexController::inscriptionPost')
+                ->post('/inscription', 'App\Controller\IndexController::inscriptionPost')
                 ->bind('weebelule_inscription_post');
 
         # On retourne la liste des controllers (ControllerCollection)
